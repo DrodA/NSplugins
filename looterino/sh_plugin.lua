54423e55fec4265d040d9340c1dcaa480dfc1235
DrodA <soderbergmilton@gmail.com>
2017-02-17 17:16:01 +0300

    Update sh_plugin.lua

--- looterino/sh_plugin.lua
+++ looterino/sh_plugin.lua
@@ -1,60 +1,21 @@
 PLUGIN.name = "Le looterino"
 PLUGIN.author = "DrodA"
-PLUGIN.desc = "le requesterino"
+PLUGIN.desc = "You leaves ur own itemerino when you riperino"
 
-netstream.Hook("_bOpen", function(entity, index)
-	local inventory = nut.item.inventories[index]
-
-	if (IsValid(entity) and inventory and inventory.slots) then
-			nut.gui.inv1 = vgui.Create("nutInventory")
-			nut.gui.inv1:ShowCloseButton(true)
-
-			local inventory2 = LocalPlayer():getChar():getInv()
-
-			if (inventory2) then
-				nut.gui.inv1:setInventory(inventory2)
-			end
-
-			local panel = vgui.Create("nutInventory")
-			panel:ShowCloseButton(true)
-			panel:SetTitle("")
-			panel:setInventory(inventory)
-			panel:MoveLeftOf(nut.gui.inv1, 4)
-			panel.OnClose = function(this)
-				if (IsValid(nut.gui.inv1) and !IsValid(nut.gui.menu)) then
-					nut.gui.inv1:Remove()
-				end
-
-				netstream.Start("invExit")
-			end
-
-			local oldClose = nut.gui.inv1.OnClose
-			nut.gui.inv1.OnClose = function()
-				if (IsValid(panel) and !IsValid(nut.gui.menu)) then
-					panel:Remove()
-				end
-
-			netstream.Start("invExit")
-			nut.gui.inv1.OnClose = oldClose
-		end
-
-		nut.gui["inv"..index] = panel
-	end
-end)
+PLUGIN.staytime = 120
 
 function PLUGIN:PlayerDeath( player, damage, attacker )
 	local character = player:getChar():getInv()
-	local model = "models/props_junk/garbage_bag001a.mdl"
 
 	local strg = ents.Create("nut_itemstorage")
-	strg:SetModel(model)
-	strg:SetPos(player:GetPos())
+	strg:SetModel("models/props_junk/garbage_bag001a.mdl")
+	strg:SetPos(player:GetPos() + Vector(0,0, 16))
 	strg:SetAngles(player:GetAngles())
 	strg:SetSolid(SOLID_VPHYSICS)
 	strg:PhysicsInit(SOLID_VPHYSICS)
 	strg.receivers = {}
 
-	nut.item.newInv(0, "itemstorage", function(inventory)
+	nut.item.newInv(0, "_itmstrg"..player:SteamID(), function(inventory)
 		if IsValid(strg) then
 			inventory:setSize(nut.config.get("invW"), nut.config.get("invH"))
 			strg:setInventory(inventory)
@@ -68,4 +29,50 @@ function PLUGIN:PlayerDeath( player, damage, attacker )
 
 		strg:Spawn()
 	end
+
+	if IsValid(strg) then
+		timer.Simple(self.staytime, function()
+			strg:Remove()
+		end)
+	end
 end
+
+netstream.Hook("_bOpen", function(entity, index)
+	local inventory = nut.item.inventories[index]
+
+	if (IsValid(entity) && inventory && inventory.slots) then
+			nut.gui.inv1 = vgui.Create("nutInventory")
+			nut.gui.inv1:ShowCloseButton(true)
+
+			local inventory2 = LocalPlayer():getChar():getInv()
+
+			if (inventory2) then
+				nut.gui.inv1:setInventory(inventory2)
+			end
+
+			local panel = vgui.Create("nutInventory")
+			panel:ShowCloseButton(true)
+			panel:SetTitle("Bag inventory")
+			panel:setInventory(inventory)
+			panel:MoveLeftOf(nut.gui.inv1, 4)
+			panel.OnClose = function(this)
+				if (IsValid(nut.gui.inv1) && !IsValid(nut.gui.menu)) then
+					nut.gui.inv1:Remove()
+				end
+
+				netstream.Start("invExit")
+			end
+
+			local oldClose = nut.gui.inv1.OnClose
+			nut.gui.inv1.OnClose = function()
+				if (IsValid(panel) && !IsValid(nut.gui.menu)) then
+					panel:Remove()
+				end
+
+			netstream.Start("invExit")
+			nut.gui.inv1.OnClose = oldClose
+		end
+
+		nut.gui["inv"..index] = panel
+	end
+end)
