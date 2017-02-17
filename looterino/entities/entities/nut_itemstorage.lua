fb940d8c397d1b83ebdb497224ac47f460ec3869
DrodA <soderbergmilton@gmail.com>
2017-02-17 12:35:09 +0300

    Add files via upload

---
+++ looterino/entities/entities/nut_itemstorage.lua
@@ -0,0 +1,72 @@
+ENT.Type = "anim"
+ENT.PrintName = "Item Storage"
+ENT.Author = "DrodA"
+ENT.Category = "NutScript"
+ENT.Spawnable = false
+
+ENT.Model = "models/Items/item_item_crate.mdl"
+
+if (SERVER) then
+	function ENT:Initialize()
+		self:SetModel(self.Model)
+		self:SetSolid(SOLID_VPHYSICS)
+		self:PhysicsInit(SOLID_VPHYSICS)
+		self:SetUseType(SIMPLE_USE)
+		self.receivers = {}
+
+		local physObj = self:GetPhysicsObject()
+
+		if (IsValid(physObj)) then
+			physObj:EnableMotion(true)
+			physObj:Wake()
+		end
+	end
+
+	function ENT:setInventory(inventory)
+		if (inventory) then
+			self:setNetVar("id", inventory:getID())
+
+			inventory.onAuthorizeTransfer = function(inventory, client, oldInventory, item)
+				if (IsValid(client) and IsValid(self) and self.receivers[client]) then
+					return true
+				end
+			end
+
+			inventory.getReceiver = function(inventory)
+				local receivers = {}
+
+				for k, v in pairs(self.receivers) do
+					if (IsValid(k)) then
+						receivers[#receivers + 1] = k
+					end
+				end
+
+				return #receivers > 0 and receivers or nil
+			end
+		end
+	end
+
+	function ENT:Use(activator)
+		local inventory = self:getInv()
+
+		if (inventory and (activator.nutNextOpen or 0) < CurTime()) then
+			if (activator:getChar()) then
+				activator:setAction("Opening...", 1, function()
+					if (activator:GetPos():Distance(self:GetPos()) <= 100) then
+						self.receivers[activator] = true
+						activator.nutBagEntity = self
+						
+						inventory:sync(activator)
+						netstream.Start(activator, "_bOpen", self, inventory:getID())
+					end
+				end)
+			end
+
+			activator.nutNextOpen = CurTime() + 1.5
+		end
+	end
+
+	function ENT:getInv()
+		return nut.item.inventories[self:getNetVar("id", 0)]
+	end
+end
---
+++ looterino/sh_plugin.lua
@@ -0,0 +1,71 @@
+PLUGIN.name = "Le looterino"
+PLUGIN.author = "DrodA"
+PLUGIN.desc = "le requesterino"
+
+netstream.Hook("_bOpen", function(entity, index)
+	local inventory = nut.item.inventories[index]
+
+	if (IsValid(entity) and inventory and inventory.slots) then
+			nut.gui.inv1 = vgui.Create("nutInventory")
+			nut.gui.inv1:ShowCloseButton(true)
+
+			local inventory2 = LocalPlayer():getChar():getInv()
+
+			if (inventory2) then
+				nut.gui.inv1:setInventory(inventory2)
+			end
+
+			local panel = vgui.Create("nutInventory")
+			panel:ShowCloseButton(true)
+			panel:SetTitle("")
+			panel:setInventory(inventory)
+			panel:MoveLeftOf(nut.gui.inv1, 4)
+			panel.OnClose = function(this)
+				if (IsValid(nut.gui.inv1) and !IsValid(nut.gui.menu)) then
+					nut.gui.inv1:Remove()
+				end
+
+				netstream.Start("invExit")
+			end
+
+			local oldClose = nut.gui.inv1.OnClose
+			nut.gui.inv1.OnClose = function()
+				if (IsValid(panel) and !IsValid(nut.gui.menu)) then
+					panel:Remove()
+				end
+
+			netstream.Start("invExit")
+			nut.gui.inv1.OnClose = oldClose
+		end
+
+		nut.gui["inv"..index] = panel
+	end
+end)
+
+function PLUGIN:PlayerDeath( player, damage, attacker )
+	local character = player:getChar():getInv()
+	local model = ""
+
+	local strg = ents.Create("nut_itemstorage")
+	--body:SetModel((model))
+	strg:SetPos(player:GetPos())
+	strg:SetAngles(player:GetAngles())
+	strg:SetSolid(SOLID_VPHYSICS)
+	strg:PhysicsInit(SOLID_VPHYSICS)
+	strg.receivers = {}
+
+	nut.item.newInv(0, "itemstorage", function(inventory)
+		if IsValid(strg) then
+			inventory:setSize(nut.config.get("invW"), nut.config.get("invH"))
+			strg:setInventory(inventory)
+		end
+	end)
+
+	local inv = character:getItems()
+	local var = strg:getNetVar("id")
+	for k, v in pairs(inv) do
+		v:transfer(var)
+
+		strg:Spawn()
+	end
+end
